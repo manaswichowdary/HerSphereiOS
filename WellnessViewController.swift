021f01c515530134b0504bbf25d0b41b2f2c2997
manaswichowdary <mmakkena@asu.edu>
2025-05-04 21:42:50 -0700

    Initial commit for HerSphere iOS app

---
+++ WellnessViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class WellnessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Basic setup if needed
+    }
+}
+
+
+
+
+
+
+
